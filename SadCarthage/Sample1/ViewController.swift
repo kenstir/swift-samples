46479315b16f73eba7a699bf04eea9c594d1e0a7
Ken Cox <kenstir@gmail.com>
2018-11-10 11:17:12 -0500

    fails to build with Xcode 10.1

---
+++ SadCarthage/Sample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample1
+//
+//  Created by Ken Cox on 11/10/18.
+//  Copyright © 2018 Ken Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
